6f4a6db87cfedd13d9bd2a1f2be3df4c5b06f76e
Sourav Paul <souravedup012@gmail.com>
2022-05-29 00:44:02 +0530

    deleted data

--- data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO msd_product(product_Id,product_Name,product_Description,price) VALUES
-(501,'phone','no 1 phone',25541),
-(502,'pen drive','fuji yama pen drive',599),
-(503,'mini player','sound box',4500.50);
